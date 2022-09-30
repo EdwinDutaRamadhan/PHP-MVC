418fa8eb20cc281c73866ad0679b11a50ffc7c27
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2022-09-30 16:28:25 +0700

    Controller added

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About{
+    public function index($nama = 'edwin', $profesi='mahasiswa'){
+        echo"Halo saya $nama saya adalah $profesi";
+    }
+    public function page(){
+        echo'About/page';
+    }
+}
