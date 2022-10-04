e9cf5c440fc60ae7dc6baeddfa2e5b647106fa32
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2022-10-04 21:57:27 +0700

    Insert Data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -2,6 +2,10 @@
 
     <div class="row">
         <div class="col-6">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+        Tambah Data Mahasiswa
+        </button>
+        <br><br>
             <h3>Daftar Mahasiswa</h3>
         </div>
     
@@ -14,4 +18,47 @@
     <?php endforeach; ?>
     </ul>
     </div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="nama" placeholder="Nama" name="nama">
+            </div>
+            <div class="mb-3">
+                <label for="nim" class="form-label">NIM</label>
+                <input type="number" class="form-control" id="nim" placeholder="Nim" name="nim">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Email.gmail.com" name="email">
+            </div>
+
+            <div class="mb-3">
+                <label for="jurusan">Jurusan</label>
+                <select class="form-select form-select-sm" aria-label=".form-select-sm example" id="jurusan" name="jurusan">
+                    <option selected>Open this select menu</option>
+                    <option value="Teknik Informatika">Teknik Infromatika</option>
+                    <option value="Sistem Informasi">Sistem Informasi</option>
+                    <option value="Desain Komunikasi Visual">Desain Komunikasi Visual</option>
+                </select>
+            </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+         </form>
+      </div>
+    </div>
+  </div>
 </div>
